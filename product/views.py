3f91ca5f37599ab1e56ba6b59ad5102ade6be5e0
adey-px <aosteph@gmail.com>
2023-08-07 23:45:24 -0500

    Added sorting and filter of products

--- product/views.py
+++ product/views.py
@@ -8,10 +8,12 @@ from .models import Category, Product
 def aLLproducts(request):
     products = Product.objects.all()
     inputName = None
-    selectedCategory = None
+    sortKeyword = None
+    sortPattern = None
+    filterCategory = None
 
-    # search by input name in search form
     if request.GET:
+        # search products by names input in search form
         if "query" in request.GET:
             inputName = request.GET["query"]
             if not inputName:
@@ -23,18 +25,41 @@ def aLLproducts(request):
                 )
                 products = products.filter(queryText)
 
-        # search by category in navbar items
-        if "category" in request.GET:
-            selectedCategory = request.GET["category"].split(",")
-            products = products.filter(category__name__in=selectedCategory)
-            selectedCategory = Category.objects.filter(name__in=selectedCategory)
+        # sort products by parameters thru navbar `PRODUCTS` link
+        if "parameter" in request.GET:
+            sortKey = request.GET["parameter"]
+            sortKeyword = sortKey
+            if sortKey == "name":
+                sortKey = "lower_name"
+                products = products.annotate(lower_name=Lower("name"))
 
+            if "pattern" in request.GET:
+                sortPattern = request.GET["pattern"]
+                if sortPattern == "descending":
+                    sortKey = f"-{sortKey}"
+            products = products.order_by(sortKey)
+
+        # filter products by category thru navbar `other links`
+        if "category" in request.GET:
+            filterCategory = request.GET["category"].split(",")
+            products = products.filter(category__name__in=filterCategory)
+            filterCategory = Category.objects.filter(name__in=filterCategory)
+
+    sortParameter = f"{sortKeyword}_{sortPattern}"
+
+    # export variables to template
     context = {
         "products": products,
-        "searchBy": inputName,
-        "sortedBy": selectedCategory,
+        "searchTxt": inputName,
+        "sortParam": sortParameter,
+        "filterCat": filterCategory,
     }
     return render(request, "product/aLLproducts.html", context)
+
+
+"""
+View for Product details
+"""
 
 
 # Product details
